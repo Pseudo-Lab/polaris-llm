27e66962e5ddb9ae366f4b5daa8e07f0b1185d1e
Brad.Min <bcmin1018@gmail.com>
2024-06-10 23:08:56 +0900

    b2ft 추가

--- chatbot/home.py
+++ chatbot/home.py
@@ -1,25 +1,9 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
 
 st.set_page_config(layout="wide")
 st.sidebar.page_link("home.py", label="Home")
 st.sidebar.page_link("pages/dataset.py", label="Dataset")
-st.sidebar.page_link("pages/prompt.py", label="Prompt")
+st.sidebar.page_link("pages/prompts.py", label="Prompt")
 st.sidebar.page_link("pages/finetuning.py", label="FT")
 st.sidebar.page_link("pages/b2ft.py", label="B2FT")
 st.sidebar.page_link("pages/llmvs.py", label="LLMvs")
-# with st.sidebar:
-#     selected = option_menu("Menu", ["Dataset", "Prompt", "FT", "B2FT", "LLMvs"],
-#         icons=['house', 'gear', 'gear', 'gear', 'gear'], menu_icon="cast", default_index=0)
-#
-#     if selected == "Prompt":
-#         st.switch_page("pages/prompt.py")
-#
-# import streamlit as st
-#
-# if st.button("Dataset"):
-#     st.switch_page("pages/dataset.py")
-# if st.button("Prompt):
-#     st.switch_page("pages/page_1.py")
-# if st.button("Page 2"):
-#     st.switch_page("pages/page_2.py")
--- chatbot/pages/prompts.py
+++ chatbot/pages/prompts.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(layout="wide")
 st.sidebar.page_link("home.py", label="Home")
 st.sidebar.page_link("pages/dataset.py", label="Dataset")
-st.sidebar.page_link("pages/prompt.py", label="Prompt")
+st.sidebar.page_link("pages/prompts.py", label="Prompt")
 st.sidebar.page_link("pages/finetuning.py", label="FT")
 st.sidebar.page_link("pages/b2ft.py", label="B2FT")
 st.sidebar.page_link("pages/llmvs.py", label="LLMvs")
